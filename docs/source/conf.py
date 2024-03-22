4a361d22eeaffc92344c633fdb35365336242e3c
cowqer <1625629469@qq.com>
2024-03-22 19:33:25 +0800

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'FUSION'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2024, Fusion'
+author = 'Fusion'
 
 release = '0.1'
 version = '0.1.0'
